38960458abb81d9b8454ebbf7efc209624d16480
Bogar8 <62038397+Bogar8@users.noreply.github.com>
2021-04-24 22:29:31 +0200

    Dodana povezava na podatkovno bazo.

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = "mongodb+srv://bogar:geslo@cluster0.iberm.mongodb.net/projektRAI?retryWrites=true&w=majority";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
